4ce57cd6b0e4d2b85386702d6f5210b9deee6b70
yangyu <yuyang239@gmail.com>
2013-01-05 22:51:31 +0800

    Clear up

--- cloudy/cloudy/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponseRedirect
-from django.contrib.auth.decorators import login_required
-
-@login_required
-def index(request):
-    return HttpResponseRedirect('/notes/')
